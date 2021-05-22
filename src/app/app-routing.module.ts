39ff9eb5021d878ccbd801f75f26d5c8aa7ee442
Eris Ristemena <e.ristemena@gmail.com>
2021-05-23 04:58:35 +0700

    Generate modules, components and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth-routing.module')
+      .then((m) => m.AuthRoutingModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
